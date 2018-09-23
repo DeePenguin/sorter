2b38820530ae878d8cb7cdaf5fbca728018f5170
DeePenguin <_deadpenguin_@mail.ru>
2018-09-23 20:09:53 +0300

    Add all features from the task

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
 class Sorter {
   constructor() {
     // your implementation
+    this._addedElements = [];
+    this.comparator = (left, right) =>  left - right;
   }
 
   add(element) {
     // your implementation
+      this._addedElements.push(element);
   }
 
   at(index) {
     // your implementation
+    return this._addedElements[index];
   }
 
   get length() {
     // your implementation
+    return this._addedElements.length;
   }
 
   toArray() {
     // your implementation
+    return this._addedElements;
   }
 
   sort(indices) {
     // your implementation
+    let tmpSortingArr = [];
+    const simpleSorter = (left, right) => left - right;
+    indices.sort(simpleSorter);
+    indices.forEach(index => tmpSortingArr.push(this._addedElements[index]));
+    tmpSortingArr.sort(this.comparator);
+    tmpSortingArr.forEach((value, index) => this._addedElements[indices[index]] = value);
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.comparator = compareFunction ? compareFunction : this.comparator;
   }
 }
 
